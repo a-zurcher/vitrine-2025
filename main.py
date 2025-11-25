b0c1f7edb405311d9ffd4abc70a4367aef5d5f24
Alexandre Zürcher <alexandre.zurcher@ik.me>
2025-11-25 17:09:18 +0100

    added ams connexion and testing endpoints

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from fastapi import FastAPI
 
+import pyads
+
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# ===============
+# PLC SETUP
+# ===============
+def write_output(value: bool, index_group: int = 0xF021, index_offset: int = 0xD0) -> bool:
+    ams_net_id = "5.98.172.87.1.1"
+    ams_port = 27905
+
+    """Writes a boolean to the output symbol and returns the readback."""
+    with pyads.Connection(ams_net_id, ams_port) as plc:
+        symbol = plc.get_symbol(
+            index_group=index_group,
+            index_offset=index_offset,
+            plc_datatype=pyads.PLCTYPE_BOOL,
+        )
+        symbol.write(value)
+        return symbol.read()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/on")
+def on():
+    return {"led_status": write_output(True)}
+
+
+@app.get("/off")
+def off():
+    return { "led_status": write_output(False) }
